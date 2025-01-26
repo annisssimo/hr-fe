3b21adaf187c4aa5003919ddfa1d37a7ba2b042f
hanna-larchanka <hanna.larchanka@sunmait.tech>
2025-01-26 13:01:55 +0300

    fix: hide menu from not auth users

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router';
-
 import * as styles from './Header.css';
 import { Logo } from '../Logo/Logo';
 import defaultUserImgPath from '../../../assets/default-avatar.jpg';
@@ -11,34 +10,42 @@ export const Header = ({ user }: HeaderProps) => {
         <header className={styles.header}>
             <div className={styles.logo}>
                 <Link to={ROUTES.HOME}>
-                    <Logo width={'120'} height={'60'} />
+                    <Logo width="120" height="60" />
                 </Link>
             </div>
-            <nav className={styles.nav}>
-                <ul className={styles.navList}>
-                    {user.role === 'admin' && (
-                        <li key="requests">
-                            <Link to={ROUTES.REQUESTS} className={styles.navItem}>
-                                Requests
-                            </Link>
-                        </li>
-                    )}
-                    <li key="database">
-                        <Link to={ROUTES.DATABASE} className={styles.navItem}>
-                            Base
-                        </Link>
-                    </li>
-                    <li key="tasks">
-                        <Link to={ROUTES.TASKS} className={styles.navItem}>
-                            Tasks
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-            <div className={styles.userProfile}>
-                <Typography variant="text">{user.username}</Typography>
-                <img src={defaultUserImgPath} alt="User Photo" className={styles.userAvatar} />
-            </div>
+            {user && (
+                <>
+                    <nav className={styles.nav}>
+                        <ul className={styles.navList}>
+                            {user.role === 'admin' && (
+                                <li key="requests">
+                                    <Link to={ROUTES.REQUESTS} className={styles.navItem}>
+                                        Requests
+                                    </Link>
+                                </li>
+                            )}
+                            <li key="database">
+                                <Link to={ROUTES.DATABASE} className={styles.navItem}>
+                                    Base
+                                </Link>
+                            </li>
+                            <li key="tasks">
+                                <Link to={ROUTES.TASKS} className={styles.navItem}>
+                                    Tasks
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={styles.userProfile}>
+                        <Typography variant="text">{user.username}</Typography>
+                        <img
+                            src={defaultUserImgPath}
+                            alt={user.username}
+                            className={styles.userAvatar}
+                        />
+                    </div>
+                </>
+            )}
         </header>
     );
 };
@@ -49,5 +56,5 @@ interface User {
 }
 
 interface HeaderProps {
-    user: User;
+    user?: User;
 }
